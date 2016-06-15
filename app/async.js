274e9c7f8a58f03324973d70e8742be26525a4f6
Pengyi Song <songpeng@gmail.com>
2016-06-15 09:38:16 -0400

    solutions

--- app/async.js
+++ app/async.js
@@ -1,11 +1,22 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
-  async: function(value) {
+	async: function(value) {
+		var defer = $.Deferred();
+		setTimeout(function() {
+			defer.resolve(value);
+		}, 1000);
+		return defer.promise();
+	},
 
-  },
-
-  manipulateRemoteData: function(url) {
-
-  }
+	manipulateRemoteData: function(url) {
+		var defer = $.Deferred();
+		$.ajax(url).then(function(res) {
+			var people = res.people.map(function(person) {
+				return person.name;
+			});
+			defer.resolve(people.sort());
+		});
+		return defer.promise();
+	}
 };
--- app/objects.js
+++ app/objects.js
@@ -1,15 +1,23 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
-  alterContext: function(fn, obj) {
+	alterContext: function(fn, obj) {
+		return fn.call(obj);
+	},
 
-  },
+	alterObjects: function(constructor, greeting) {
+		constructor.prototype.greeting = greeting;
+	},
 
-  alterObjects: function(constructor, greeting) {
+	iterate: function(obj) {
 
-  },
+		var listOfProp = [];
 
-  iterate: function(obj) {
-
-  }
+		for (var prop in obj) {
+			if (obj.hasOwnProperty(prop)) {
+				listOfProp.push(prop + ':' + obj[prop]);
+			}
+		}
+		return listOfProp;
+	}
 };
